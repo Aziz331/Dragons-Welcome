3f0ba1a95eae46280be27865ac77a8e844445f87
Aziz331 <44896829+Aziz331@users.noreply.github.com>
2018-11-24 04:58:26 +0700

    Add files via upload

---
+++ bot.js.js
@@ -0,0 +1,27 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ const prefix = "??????";
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'zg') {
+        message.reply('pong');
+      }
+});
+
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***???? ????????? ?????***" ,member.user.username )
+    .setDescription('***??? ?? ??????? ???? ??????? ?????? ?? ???? ???? ??????? ??????? ????***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel =member.guild.channels.find('name', 'chat-arabjoker')
+if (!channel) return;
+channel.send({embed : embed});
+});
+
+client.login(process.env.BOT_TOKEN);
